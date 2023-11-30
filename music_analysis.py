eba3c5fa8ecd21e194551a595008a634d2c3adbf
MarinEstrada <adrian.marin.estrada@gmail.com>
2023-11-29 22:26:39 -0800

    ADD: put acquired data into df and see what data is valid

--- music_analysis.py
+++ music_analysis.py
@@ -4,10 +4,16 @@ import sys
 import numpy as np
 import pandas as pd
 
-def main(zip_file = "rap_archive.zip"):
+def main(rap_archive = "rap_archive.zip", data_acquired = "data-1.csv.gz"):
 
-    music_data = pd.read_csv(zip_file)
-    print(f"rap_data is:\n{music_data}")
+    # music_data = pd.read_csv(rap_archive)
+    music_data = pd.read_csv(data_acquired)
+    print(f"data_acquired is:\n{music_data}")
+    music_data = music_data[music_data['status_code']==200]
+    print(f"data_acquired with succesful API calls:\n{music_data}")
+    music_data = music_data.dropna()
+    print(f"valid data_acquired is:\n{music_data}")
+
 
     # TODO: access spotify API
 
