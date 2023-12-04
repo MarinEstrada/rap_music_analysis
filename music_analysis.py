7b6437e3bd5ffc3aa7723f3655e19a013944e94f
Boey Kwan <boey.kwan7@gmail.com>
2023-12-03 18:29:21 -0800

    most frequent content words

--- music_analysis.py
+++ music_analysis.py
@@ -8,42 +8,54 @@ import matplotlib.pyplot as plt
 import copy
 import nltk
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
 stopwords_eng = set(stopwords.words('english')) # as shown in https://www.geeksforgeeks.org/removing-stop-words-nltk-python/
-lemmatizer = WordNetLemmatizer()
+lemmatizer = WordNetLemmatizer() # as shown in https://www.geeksforgeeks.org/python-lemmatization-with-nltk/
 
 # Splits string by whitespace
 def split(text):
     return text.split()
 split_ufunc = np.frompyfunc(split, 1, 1)
 
+# Lemmatizes
+def lemmatize(word):
+    return lemmatizer.lemmatize(word)
+lemmatize_ufunc = np.frompyfunc(lemmatize, 1, 1)
+
 # Uses ufunc to tokenize lyrics and put one token per row
 def tokenize(song_data):
     words = copy.deepcopy(song_data)
     words['lyric'] = split_ufunc(words['lyric'])
     words = words.explode('lyric')
+    words['lyric'] = lemmatize_ufunc(words['lyric'])
     return words
 
 # Puts only unique words per song on a new row
 def get_unique_words(words_data):
     words = copy.deepcopy(words_data)
-    words['unique word count'] = np.ones(words.shape[0])
-    cols = [c for c in words.columns if c != 'unique word count']
-    unique_words = words.groupby(cols, as_index=False).agg({'unique word count':'sum'})
+    words['frequency'] = np.ones(words.shape[0])
+    cols = [c for c in words.columns if c != 'frequency']
+    unique_words = words.groupby(cols, as_index=False).agg({'frequency':'sum'})
     return unique_words
 
 # Divides a col by the minutes column
 def wpm(df, col):
     return df[col] / df['minutes']
 
-# takes list of tokens and removes stopwords, remaining words are lemmatized
+# takes list of tokens and removes stopwords
 def stop_word_removal(tokens):
-    # return [item for item in tokens if item not in stopwords_eng]
-    return [lemmatizer.lemmatize(item) for item in tokens if item not in stopwords_eng] #added lemmatization
+    return [item for item in tokens if item not in stopwords_eng]
+
+def drop_stopword(word):
+    if word not in stopwords_eng:
+        return word
+    else:
+        return pd.NA
+drop_stopword_ufunc = np.frompyfunc(drop_stopword, 1, 1)
 
 # # uses nltk to tokenize
 # def tokenize_lyrics(df, lyric_column, tokenized_name):
@@ -87,6 +99,11 @@ def main(rap_archive = "rap_archive.zip", api_data = "data-1.csv.gz", output_fil
     # print(f"words dataframe is:\n{words}")
     # print(f"unique_words dataframe is:\n{unique_words}")
 
+    # boey code: content words
+    content_words = copy.deepcopy(words)
+    content_words['lyric'] = drop_stopword_ufunc(content_words['lyric'])
+    content_words = content_words.dropna()
+
     # boey code: count words
     word_count = words.groupby([c for c in words.columns if c != 'lyric'], as_index=False).count()
     word_count = word_count.rename(columns={'lyric':'word count'})
@@ -99,33 +116,31 @@ def main(rap_archive = "rap_archive.zip", api_data = "data-1.csv.gz", output_fil
     song_data = song_data.merge(unique_word_count)
     # print(f"song_data is:\n{song_data}")
 
+    # boey code: count content words
+    content_word_count = content_words.groupby([c for c in content_words.columns if c != 'lyric'], as_index=False).count()
+    content_word_count = content_word_count.rename(columns={'lyric':'content word count'})
+    song_data = song_data.merge(content_word_count)
+    # print(f"song_data is:\n{song_data}")
+
     # boey code: words and unique words per minute
     song_data['words per minute'] = wpm(song_data, 'word count')
     song_data['unique words per minute'] = wpm(song_data, 'unique word count')
+    song_data['lexical density'] = song_data['content word count'] / song_data['word count']
     # song_data = song_data.sort_values('words per minute')
     song_data = song_data[song_data['words per minute'] <= 450] # prune songs over 450 words/min
     # print(f"song_data is:\n{song_data}")
 
-    # adri code:
-    # song_data = tokenize_lyrics(song_data, 'lyric', 'tokenized_lyric')
-    song_data['tokenized'] = song_data.apply(lambda item: word_tokenize(item['lyric']), axis=1) #tokenization of lyrics as seperate column
-    # print(f"song_data is:\n{song_data}")
-    song_data['tokenized'] = song_data.apply(lambda item: stop_word_removal(item['tokenized']), axis=1) #removes stopwords from tokens
-    # print(f"song_data is:\n{song_data}")
-    song_data['unique_words'] = song_data.apply(lambda item: set(item['tokenized']), axis=1) #by changing list of tokens to set we get unique words
-    # print(f"song_data is:\n{song_data}")
-
-    exit()
     # boey code: most frequent content words
-    percentile = 80
-    content_words = unique_words[unique_words['lyric'].str not in stopwords_eng]
-    grouped = unique_words.groupby([c for c in unique_words.columns if c not in ['lyric','unique word count']], as_index=False)
-    most_frequent_content_words = grouped.agg({'unique word count':'max'})
-    most_frequent_content_words = most_frequent_content_words.rename(columns={'unique word count':'max unique word count'})
-    most_frequent_content_words = most_frequent_content_words.merge(unique_words)
-    most_frequent_content_words['relative word count'] = most_frequent_content_words['unique word count'] / most_frequent_content_words['max unique word count']
-    most_frequent_content_words = most_frequent_content_words[most_frequent_content_words['relative word count'] > (percentile/100)]
-    print(most_frequent_content_words)
+    percentile = 75.0
+    unique_content_words = get_unique_words(content_words)
+    grouped = unique_content_words.groupby([c for c in unique_content_words.columns if c not in ['lyric','frequency count']], as_index=False)
+    unique_content_words['word count rank'] = grouped['frequency'].rank(pct=True)
+    top_content_words = unique_content_words[unique_content_words['word count rank'] >= (percentile/100.0)]
+    top_content_words = top_content_words.sort_values('word count rank',ascending=True)
+    # print(f"top_content_words is:\n{top_content_words}")
+
+    # justin code: sentiment analysis
+    # song_data['sentiment score'] <-- 
 
 
 if __name__ == '__main__':
